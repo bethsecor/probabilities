630113fdaa32c0801728fa345341981409481c5a
Beth Secor <elizabeth.a.secor@gmail.com>
2016-07-17 12:16:17 -0600

    Show odds ratio results

--- test/controllers/app_test.rb
+++ test/controllers/app_test.rb
@@ -8,9 +8,24 @@ class AppTest < Minitest::Test
     Sinatra::Application
   end
 
-  def test_hello_world
+  def test_two_by_two_form_page
     get '/'
     assert last_response.ok?
-    assert_equal "Hello\n", last_response.body
+    form = "<form method='get' action='/results'>
+  <input type='text' name='a'>
+  <input type='text' name='b'>
+  <br>
+  <input type='text' name='d'>
+  <input type='text' name='c'>
+  <br>
+  <input type='submit'>
+</form>\n"
+    assert_equal form, last_response.body
+  end
+
+  def test_results
+    get '/results', params: {a: 1, b: 6, c: 3, d: 2}
+    assert last_response.ok?
+    assert_equal "OR: 0.25\n", last_response.body
   end
 end
